710d0ccafccc9224874ed6a5fb528c11dda9e788
Vishakha Mulik <140291651+mulikvi@users.noreply.github.com>
2024-10-03 06:38:11 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll functionality for anchor links
+document.querySelectorAll('.menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
